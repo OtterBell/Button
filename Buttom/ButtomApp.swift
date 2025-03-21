a69ef97b1387396fc8f20cbff9d5eedfd5c73d47
OtterBell <r08625028@ntu.edu.tw>
2025-03-21 11:35:20 +0800

    Initial Commit

---
+++ Buttom/ButtomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtomApp.swift
+//  Buttom
+//
+//  Created by Mac11 on 2025/3/21.
+//
+
+import SwiftUI
+
+@main
+struct ButtomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buttom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Buttom
+//
+//  Created by Mac11 on 2025/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
